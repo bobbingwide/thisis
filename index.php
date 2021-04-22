4e54972444ffb97b7fc493c9452c305e26473ae5
Herb Miller <herb@bobbingwide.com>
2021-04-22 18:44:21 +0100

    Issue #2 - This is... probably the minimum mandatory content for an experimental FSE theme with Gutenberg 10.4.1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Apparently this can be empty.
+_e( 'Activate Gutenberg to use the "This is" theme', 'thisis');
+?>
+    <br />
+<?php
+_e( 'You also need an index.html file in block-templates', 'thisis');
+
+
