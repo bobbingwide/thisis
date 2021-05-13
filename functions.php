97d5d9514e88bf17a548060087940020ff457416
Herb Miller <herb@bobbingwide.com>
2021-05-13 17:05:56 +0100

    Issue #29 - remove unnecessary code for [post-edit] shortcode, replaced by oik-sb/sb-post-edit-block

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Add theme supports not yet supported by experimental-theme.json
+ * Add theme supports not yet supported by theme.json
  *
  * So far that's:
  * - post-thumbnails
@@ -25,7 +24,7 @@ function thisis_support() {
 	add_theme_support( 'responsive-embeds' );
 
 	// Add support for editor styles.
-	//add_theme_support( 'editor-styles' );
+	// add_theme_support( 'editor-styles' );
 
 	// Enqueue editor styles.
 	// @TODO Is it a good idea or a bad idea to use style.css as the editor style
@@ -59,25 +58,17 @@ function thisis_get_theme_version() {
 }
 
 /**
- * Enqueue scripts and styles.
+ * Enqueue theme stylesheet.
  */
 function thisis_scripts() {
-
-	// Enqueue theme stylesheet.
-
 	$version=thisis_get_theme_version();
-	wp_enqueue_style( 'sb-style', get_template_directory_uri() . '/style.css', array(), $version );
-
-	// Enqueue alignments stylesheet.
-	//wp_enqueue_style( 'sb-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'thisis-style', get_template_directory_uri() . '/style.css', array(), $version );
 }
-
 
 function thisis_loaded() {
 	add_action( 'after_setup_theme', 'thisis_support' );
 	add_action( 'wp_enqueue_scripts', 'thisis_scripts' );
 	add_action( 'init', 'thisis_init', 20 );
-
 }
 
 /**
@@ -86,41 +77,10 @@ function thisis_loaded() {
 function thisis_init() {
 	if ( function_exists( 'bw_add_shortcode' ) ) {
 		do_action( "oik_add_shortcodes" );
-
 	} else {
 		// oik shortcodes won't be expanded.
 	}
-	//add_shortcode( 'archive_description', 'fizzie_archive_description' );
-	add_shortcode( 'post-edit', 'thisis_post_edit' );
 }
-
-
-
-
-
-/**
- * Implements [post-edit] shortcode.
- *
- * If the user is authorised return a post edit link for the current post.
- *
- * @param $attrs
- * @param $content
- * @param $tag
- *
- * @return string
- */
-function thisis_post_edit( $attrs, $content, $tag ) {
-	$link='';
-	$url =get_edit_post_link();
-	if ( $url ) {
-		$class='bw_edit';
-		$text =__( '(Edit)', 'sb' );
-		$link ='<a class="' . esc_attr( $class ) . '" href="' . esc_url( $url ) . '">' . $text . '</a>';
-	}
-
-	return $link;
-}
-
 require_once __DIR__ . '/includes/block-overrides.php';
 
 thisis_loaded();
